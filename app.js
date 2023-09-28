04c322b985b56f0589322444373050c1d0b690d3
TruongNguyenLoi <dragonst18@gmail.com>
2023-09-28 15:46:57 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const ApiError = require("./app/api-error");
+
 
 const app = express();
 const contactsRouter = require("./app/routes/contact.route");
@@ -12,5 +14,17 @@ app.get("/", (req, res) => {
 	res.json({message: "welcome to conteact book application." });
 });
 app.use("/api/contacts", contactsRouter);
+app.use("/api/contacts", contactsRouter);
+app.use((req, res, next) => {
+	return next(new ApiError(404, "Resource not found"));
+});
+
+app.use((err, req, res, next) => {
+	return res.status(error.statusCode || 500).json({
+		message: error.message || "Internal Server Error",
+	});
+});
+
+
 
 module.exports = app;
